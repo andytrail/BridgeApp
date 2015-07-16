bd0909b015c7dbdd1aa900c66978b06ebbf82e4c
andytrail <dbailey2006@gmail.com>
2015-07-16 13:41:14 -0700

    3

--- program.js
+++ program.js
@@ -4,15 +4,19 @@
 //console.log(world);
 
 
-
+var total = 0;
 for (var i = 0; i < process.argv.length; i++){
 
 if (i >= 2) {
 console.log(Number(process.argv[i]));
+    total += i;
+
 
 }
     else {
     console.log('not a number');
 
     }
+console.log(total);
+
 }

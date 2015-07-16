db1ae3f562259eed5060cd114058d897e2bb3408
andytrail <dbailey2006@gmail.com>
2015-07-16 14:32:44 -0700

    3

--- program.js
+++ program.js
@@ -24,10 +24,12 @@ console.log(total);*/
 //exercise 3
 var line = 0;
 var fs = require('fs');
-console.log(process.argv[2]);
+//console.log(process.argv[2]);
 var buf = fs.readFileSync(process.argv[2]);
 var str = buf.toString();
-line = str.split('\n');
+line = [str.split('\n')];
 console.log(line.length);
+console.log(line[1]);
+
 
 

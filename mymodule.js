9a864d4da6b885cf27e149ab02fa9e486e487591
andytrail <dbailey2006@gmail.com>
2015-07-17 15:11:33 -0700

    12

---
+++ mymodule.js
@@ -0,0 +1,17 @@
+var exports = module.exports = {}
+var fs = require('fs');
+var path = require('path');
+
+function countext(callback) {
+exports.fs.readdir(process.argv[2], function getcount(err, contents){
+if (err) return callback(err);
+for (var i = 0; i < contents.length; i++){
+var ptc = path.extname(contents[i]);
+if (ptc == '.'+process.argv[3]) {
+
+     console.log(contents[i]);
+    }
+    }
+
+});
+}

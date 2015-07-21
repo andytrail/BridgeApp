f2d255846cab37f689c97f25a7e92e36230595d5
andytrail <dbailey2006@gmail.com>
2015-07-21 11:27:40 -0700

    12

--- mymodule.js
+++ mymodule.js
@@ -4,7 +4,7 @@ module.exports = function (dir, ext, callback){
 var fs = require('fs')
 var path = require('path');
 
-var results = dir+'  '+ext;
+var results = 'This is some text';
           return results;
 callback(null, results);
 };

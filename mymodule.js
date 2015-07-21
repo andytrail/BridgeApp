c9621d062e07f1f86ddcb0ef956ddcf37201f2f9
andytrail <dbailey2006@gmail.com>
2015-07-21 08:19:13 -0700

    12

--- mymodule.js
+++ mymodule.js
@@ -2,7 +2,7 @@
 var fs = require('fs')
 var path = require('path')
 
-module.exports.calc = function (callback){
+module.exports.calc = function (dir, ext, callback){
 
 fs.readdir(dir, function (err, list) {
       list.forEach(function (file) {

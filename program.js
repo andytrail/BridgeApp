677b06a35002bf743a4997e3e1458b7f71f8562b
andytrail <dbailey2006@gmail.com>
2015-07-21 13:27:34 -0700

    12

--- program.js
+++ program.js
@@ -140,6 +140,7 @@ var http = require('http');
 
 http.get(process.argv[2], function (res) {
 console.log(res.statusCode);
+    res.setEncoding('utf8');
     res.on("data", function(chunk) {
         console.log(chunk);
     })

742c5b1a6940674c288a5c87f8f9422731fc211c
andytrail <dbailey2006@gmail.com>
2015-07-21 12:04:32 -0700

    12

--- mymodule.js
+++ mymodule.js
@@ -4,13 +4,12 @@ module.exports = function (dir, ext, callback){
 var fs = require('fs')
 var path = require('path');
 var rest = [dir, ext, 'this is the text'];
-fs.readdir(dir, function getcount(err, contents){
-for (var i = 0; i < contents.length; i++){
-var ptc = path.extname(contents[i]);
-if (ptc == '.'+ext) {
-    rest.push(contents);
-}
-}
+     fs.readdir(dir, function (err, list) {
+      list.forEach(function (file) {
+        if (path.extname(file) === '.' + ext)
+          rest.push(file)
+         }
+                   }
 }
 callback(null, rest);
 };
